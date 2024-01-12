d73294e5741f57149cbd1e0ae7bd4131a499fc70
Joao Holanda <joaocmdt505@gmail.com>
2024-01-12 14:39:11 -0300

    atualiza os registros no banco de dados e no app

--- todo.py
+++ todo.py
@@ -12,7 +12,9 @@ class ToDo:
     self.page.window_always_on_top = True
     self.page.title = 'ToDo App'
     self.task = ''
+    self.view = 'all'
     self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name, status)')
+    self.results = self.db_execute('SELECT * FROM tasks')
     self.main_page()
 
   def db_execute(self, query, params = []):
@@ -21,13 +23,32 @@ class ToDo:
       cur.execute(query, params)
       con.commit()
       return cur.fetchall()
+    
+  def checked(self, e):
+    is_checked = e.control.value
+    label = e.control.label
+
+    if is_checked:
+      self.db_execute('UPDATE tasks SET status = "complete" WHERE name = ?', params=[label])
+    else:
+      self.db_execute('UPDATE tasks SET status = "incomplete" WHERE name = ?', params=[label])
+
+    if self.view == 'all':
+      self.results = self.db_execute('SELECT * FROM tasks')
+    else:
+      self.results = self.db_execute('SELECT * FROM tasks WHERE status = ?', params=[self.view])
+      
+    self.update_task_list()
 
   def tasks_container(self):
     return ft.Container(
       height=self.page.height * 0.8,
       content = ft.Column(
         controls=[
-          ft.Checkbox(label='Tarefa1', value=True)
+          ft.Checkbox(label=res[0], 
+                      on_change = self.checked,
+                      value=True if res[1] == 'complete' else False)
+          for res in self.results if res
         ]
       )
     )
@@ -42,6 +63,14 @@ class ToDo:
     if name:
       self.db_execute(query='INSERT INTO tasks VALUES(?,?)', params=[name, status])
       input_task.value = ''
+      self.results = self.db_execute('SELECT * FROM tasks')
+      self.update_task_list()
+
+  def update_task_list(self):
+    tasks = self.tasks_container()
+    self.page.controls.pop()
+    self.page.add(tasks)
+    self.page.update()
 
   def main_page(self):
     input_task = ft.TextField(
